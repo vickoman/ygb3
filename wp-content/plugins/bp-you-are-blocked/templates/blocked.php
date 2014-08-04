8c97e621eab6122a7120cdfc202c1a714b5e1f82
Martin <nitromax3000@hotmail.com>
2014-08-04 00:36:44 -0500

    modificado plantilla usuario bloqueado

--- wp-content/plugins/bp-you-are-blocked/templates/blocked.php
+++ wp-content/plugins/bp-you-are-blocked/templates/blocked.php
@@ -1,16 +1,16 @@
 <?php get_header( 'buddypress' ); ?>
-
-	<div id="content">
+<?php get_sidebar( 'buddypress' ); ?>
+	<div class="col-lg-9 col-md-8">
 		<div class="padder">
 			<!-- You are blocked! Template -->
 			<div id="buddypress">
 				<p>&nbsp;</p>
-				<h1 style="text-align:center;"><?php _e( 'Blocked Profile', 'bpblock' ); ?></h1>
-				<h2 style="text-align:center;"><?php _e( 'You have selected to block this profile', 'bpblock' ); ?></h2>
-				<p style="text-align:center;"><a href="<?php echo bp_loggedin_user_domain() . 'settings/blocked/'; ?>" class="button button-large"><?php _e( 'Manage Blocked Users', 'bpblock' ); ?></a><br /><br /><br /><br /><br /></p>
+				<h1 style="text-align:center;"><?php _e( 'Perfil Bloqueado', 'bpblock' ); ?></h1>
+				<h2 style="text-align:center;"><?php _e( 'Tu has seleccionado bloquear este perfil', 'bpblock' ); ?></h2>
+				<p style="text-align:center;"><a href="<?php echo bp_loggedin_user_domain() . 'settings/blocked/'; ?>" class="button button-large"><?php _e( 'Administrar usuarios bloqueados', 'bpblock' ); ?></a><br /><br /><br /><br /><br /></p>
 			</div>
 		</div>
 	</div>
 
-<?php get_sidebar( 'buddypress' ); ?>
+
 <?php get_footer( 'buddypress' ); ?>
