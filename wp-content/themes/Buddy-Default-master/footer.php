852a7c814b3388706c9c9b07bcdac215fdfe99de
Victor Manuel <vicmandlagasca@gmail.com>
2014-08-04 11:50:37 -0500

    Quitar Footer

--- wp-content/themes/Buddy-Default-master/footer.php
+++ wp-content/themes/Buddy-Default-master/footer.php
@@ -4,23 +4,7 @@
 
 		<?php do_action( 'bp_after_container' ); ?>
 		<?php do_action( 'bp_before_footer'   ); ?>
-</div>
-		<div id="footer">
-<a href="<?php bloginfo('url'); ?>" alt="Home" title="Inicio">Yogobierno 2014</a>
-			<?php if ( is_active_sidebar( 'first-footer-widget-area' ) || is_active_sidebar( 'second-footer-widget-area' ) || is_active_sidebar( 'third-footer-widget-area' ) || is_active_sidebar( 'fourth-footer-widget-area' ) ) : ?>
-				<div id="footer-widgets">
-					<?php get_sidebar( 'footer' ); ?>
-				</div>
-			<?php endif; ?>
-
-			<div id="site-generator" role="contentinfo">
-				<?php do_action( 'bp_dtheme_credits' ); ?>
-
-			</div>
-
-			<?php do_action( 'bp_footer' ); ?>
-
-		</div><!-- #footer -->
+</div>		
 
 		<?php do_action( 'bp_after_footer' ); ?>
 
