35a9f118129b6c57c23ffa1e5c1617a52df6be58
Victor Manuel <vicmandlagasca@gmail.com>
2014-08-10 15:04:08 -0500

    Nueva config database

--- aplicativo/application/models/aplicativomodel.php
+++ aplicativo/application/models/aplicativomodel.php
@@ -5,17 +5,18 @@
 */
 class Aplicativomodel extends CI_Model
 {
-	$db_mario = '';
-	$db_pepa = '';
-	public function __constructor()
-	{
-		parent::__constructor();
-		$this->db_mario = $this->load->database('default', TRUE);
-		$this->db_pepa  = $this->load->database('yogopepa', TRUE);
-	}
+	// $db_mario = '';
+	// $db_pepa = '';
+	// public function __constructor()
+	// {
+	// 	parent::__constructor();
+	// 	$this->db_mario = $this->load->database('default', TRUE);
+	// 	$this->db_pepa  = $this->load->database('yogopepa', TRUE);
+	// }
 	public function getlistado()
 	{
-		$query = $this->db_mario->query('select * from yogobierno3.yg_registro limit 3');
+		$db_mario = $this->load->database('default', TRUE);
+		$query = $db_mario->query('select * from yogobierno3.yg_registro limit 3');
 		return $query->result();
 	}
 }
